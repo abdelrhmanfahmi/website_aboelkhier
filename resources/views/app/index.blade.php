506c305a62ab748d7e23af44c6a000d754bf4de0
aitsystemsgit <abdo.fahmy9696@gmail.com>
2024-07-13 14:02:23 +0300

    changes second part 2

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="shortcut icon" href="{{asset('assets/images/firstLogo.JPG')}}"  />
-    <title>AboElKher</title>
+    <title>AboElKhier</title>
     <style>
         html, body {
             margin: 0;
